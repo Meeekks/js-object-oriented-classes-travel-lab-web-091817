6427f21222574e88cb59c8e9af21f49ec32ae7c9
github username <github email address>
2017-11-01 03:32:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return (endDate - this.startDate.getFullYear() - 1);
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let blocks = 0;
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+    if (this.beginningLocation.vertical > this.endingLocation.vertical) {
+      blocks += (this.beginningLocation.vertical - this.endingLocation.vertical);
+    } else {
+      blocks += (this.endingLocation.vertical - this.beginningLocation.vertical);
+    }
+
+    if (eastWest.indexOf(this.beginningLocation.horizontal) > eastWest.indexOf(this.endingLocation.horizontal)) {
+      blocks += (eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    } else {
+      blocks += (eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal));
+    }
+
+    return blocks;
+  }
+
+
+  estimatedTime(peak) {
+    let blocks = this.blocksTravelled()
+    if (peak === true) {
+      return blocks/2
+    } else {
+      return blocks/3
+    }
+  }
+
+}
